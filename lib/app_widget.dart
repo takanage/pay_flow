baf6d7f12dc5f3ea534e2132356596c114143dfc
Willian Takanage <williantakanage@gmail.com>
2021-06-24 22:46:01 -0300

    Created Barcode Scanner Page

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
@@ -7,6 +9,13 @@ import 'modules/login/login_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -18,10 +27,10 @@ class AppWidget extends StatelessWidget {
       ),
       initialRoute: "/splash",
       routes: {
-        "/splash" : (context) => SplashPage(),
-        "/home" : (context) => HomePage(),
-        "/login" : (context) => LoginPage(),
-        "/barcode_scanner" : (context) => BarcodeScannerPage(),
+        "/splash": (context) => SplashPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
       },
     );
   }
--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
-import 'package:payflow/shared/widgets/social_login/label_button/label_button.dart';
+import 'package:payflow/shared/widgets/bottom_sheet/bottom_sheet_widget.dart';
+import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
   const BarcodeScannerPage({Key? key}) : super(key: key);
@@ -13,36 +14,60 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return BottomSheetWidget(
+      title: "Não foi possível identificar um código de barras.",
+      subtitle:"Tente escanear novamente ou digite o código do seu boleto.",
+      primaryLabel: "Escanear novamente",
+      primaryOnPressed: () {},
+      secondaryLabel: "Digitar código",
+      secondaryOnPressed: () {},
 
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          "Escaneie o código de barras do boleto",
-          style: TextStyles.buttonBackground,
-        ),
-        leading: BackButton(
-          color: AppColors.background,
+
+    );
+    return SafeArea(
+      top: true,
+      bottom: true,
+      left: true,
+      right: true,
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Text(
+              "Escaneie o código de barras do boleto",
+              style: TextStyles.buttonBackground,
+            ),
+            centerTitle: true,
+            leading: BackButton(
+              color: AppColors.background,
+            ),
+          ),
+          body: Column(
+            children: [
+              Expanded(
+                  child: Container(
+                color: Colors.black,
+              )),
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.transparent,
+                  )),
+              Expanded(
+                  child: Container(
+                color: Colors.black,
+              )),
+            ],
+          ),
+          bottomNavigationBar: SetLabelButtons(
+            primaryLabel: "Inserir código do boleto",
+            primaryOnPressed: () {},
+            secondaryLabel: "Adcionar da galeria",
+            secondaryOnPressed: () {},
+          ),
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-              child: Container(
-            color: Colors.black,
-          )),
-          Expanded(
-            flex: 2,
-              child: Container(
-            color: Colors.transparent,
-          )),
-          Expanded(
-              child: Container(
-            color: Colors.black,
-          )),
-        ],
-      ),
-      bottomNavigationBar: LabelButton(label: "Inserir código do boleto", onPressed: (){}),
     );
   }
 }
